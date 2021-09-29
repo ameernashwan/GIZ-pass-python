f5903c5c382ba65b68a44024519d9001f65040a9
ameernashwan <78375246+ameernashwan@users.noreply.github.com>
2021-09-29 17:02:07 +0300

    Update python-pass.py

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,28 @@
-
-
+s = "babad"
 class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+     res = ""
+     resLen = 0
+     for i in range(len(s)):
+        # odd length
+         l , r = i,i
+         while l >= 0 and r < len(s) and s[l] == s[r]:
+             if ( r - l + 1 ) > resLen :
+                 res = s [l:r+1]
+                 resLen = r - l + 1
+             l -= 1
+             r += 1
+         # even length
+         l , r = i,i + 1
+         while l >= 0 and r < len(s) and s[l] == s[r]:
+               if (r - l + 1 ) > resLen :
+                    res = s[l:r+1]
+                    resLen = r - 1 + 1
+               l -= 1
+               r += 1
+     return res
+
+result = Solution.longest_palindromic(s)
+print(result)
